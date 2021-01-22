5ac131c98ba6ec7d572a0a0b4c726473e230777a
Iyadchafroud <60520088+Iyadchafroud@users.noreply.github.com>
2021-01-22 11:16:40 +0100

    make it better

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,115 @@
+import React,{useContext,useRef,useEffect,useState} from 'react'
+import {Link ,useHistory} from 'react-router-dom'
+import {UserContext} from '../App'
+import M from 'materialize-css'
+import CretePost from './screens/CreatePost'
+import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
+
+
+const NavBar = ()=>{
+    const  searchModal = useRef(null)
+    const [search,setSearch] = useState('')
+    const [userDetails,setUserDetails] = useState([])
+     const {state,dispatch} = useContext(UserContext)
+     const history = useHistory()
+     useEffect(()=>{
+         M.Modal.init(searchModal.current)
+     },[])
+     const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+     const renderList = ()=>{
+       if(state){
+           return [<div  >
+                   
+
+            <li> <i  data-target="modal1" className="large material-icons modal-trigger" style={{color:"black"}}>search</i></li>
+              
+                
+                
+
+           <li><Link to="/profile">Profile</Link></li> 
+           <li>
+               <i>                <AddBoxOutlinedIcon onClick={handleShow} > </AddBoxOutlinedIcon>
+</i>
+                <CretePost handleClose={handleClose} show={show} /></li> 
+
+           <li><Link to="/myfollowingpost">My following Posts</Link></li> 
+            
+            <li> <button className="btn #c62828 red darken-3"
+            onClick={()=>{
+              localStorage.clear()
+              dispatch({type:"CLEAR"})
+              history.push('/signin')
+            }}
+            >
+                Logout
+            </button>
+            
+         </li>
+         </div>
+            
+           ]
+       }else{
+         return [
+          <li  key="6"><Link to="/signin">Signin</Link></li>,
+          <li  key="7"><Link to="/signup">Signup</Link></li>
+         
+         ]
+       }
+     }
+
+
+     const fetchUsers = (query)=>{
+        setSearch(query)
+        fetch('/search-users',{
+          method:"post",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify({
+            query
+          })
+        }).then(res=>res.json())
+        .then(results=>{
+          setUserDetails(results.user)
+        })
+     }
+       return(
+        <nav  >
+        <div className="nav-wrapper white">
+          <Link to={state?"/":"/signin"} className="brand-logo left">Instagram</Link>
+          <ul id="nav-mobile" className="right">
+             {renderList()}
+  
+          </ul>
+        </div>
+        <div id="modal1" class="modal" ref={searchModal} style={{color:"black"}}>
+          <div className="modal-content">
+          <input
+            type="text"
+            placeholder="search users"
+            value={search}
+            onChange={(e)=>fetchUsers(e.target.value)}
+            />
+             <ul className="collection">
+               {userDetails.map(item=>{
+                 return <Link to={item._id !== state._id ? "/profile/"+item._id:'/profile'} onClick={()=>{
+                   M.Modal.getInstance(searchModal.current).close()
+                   setSearch('')
+                 }}><li className="collection-item">{item.email}</li></Link> 
+               })}
+               
+              </ul>
+          </div>
+          <div className="modal-footer">
+            <button className="modal-close waves-effect waves-green btn-flat" onClick={()=>setSearch('')}>close</button>
+          </div>
+        </div>
+      </nav>
+    )
+}
+
+
+export default NavBar
---
+++ client/src/components/screens/SignIn.js
@@ -0,0 +1,165 @@
+import React,{useState,useContext,} from 'react'
+import {Link,useHistory} from 'react-router-dom'
+import {UserContext} from '../../App'
+import M from 'materialize-css'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { constants } from 'buffer';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+ const SignIn=()=> {
+  const classes = useStyles();
+     const {state,dispatch} = useContext(UserContext)
+    const history = useHistory()
+    const [password,setPasword] = useState("")
+    const [email,setEmail] = useState("")
+
+    
+    
+
+    const PostData = ()=>{
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+            return (
+
+             
+            
+            M.toast({html: "invalid email",classes:"#c62828 red darken-3"}))  
+        }
+        fetch("/signin",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                password,
+                email
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+           if(data.error){
+              M.toast({html: data.error,classes:"#c62828 red darken-3"})
+           }
+           else{
+               localStorage.setItem("jwt",data.token)
+               localStorage.setItem("user",JSON.stringify(data.user))
+               dispatch({type:"USER",payload:data.user})
+               M.toast({html:"signedin success",classes:"#43a047 green darken-1"})
+               history.push('/')
+           }
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+             value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e)=>setPasword(e.target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={()=>PostData()}
+
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link to="/reset" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+<Link to="/signup"variant="body2">
+                "Don't have an account? Sign Up"
+              </Link>
+            </Grid>
+          </Grid>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+
+
+export default SignIn
