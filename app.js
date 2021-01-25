18079e153dfc1d01b36d3a7b2476468f04e28272
Iyadchafroud <60520088+Iyadchafroud@users.noreply.github.com>
2021-01-25 13:11:11 +0100

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
+const {MONGO__URI} = require('./config/keys')
 
 
 mongoose.connect(MONGOURI,{
